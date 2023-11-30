0527f64f66fbecea800d8dc053ccc107152575c9
MartiRF <martinreyesrf@gmail.com>
2023-11-30 12:09:09 -0600

    App.tsx: Agrege proyecto y estilos

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ function App() {
 
   return (
     <div className="container flex flex-col">
-      <h1 className='content-center text-5xl font-mono font-bold'>Portafolio</h1>
-      <p className='text-start'><strong>Jose Martin Reyes Franco</strong></p>
-      <p className='text-justify'>Aqui voy a poner mis proyecto que ire haciendo como desarrolador, para llevar un historial.</p>
+      <div className='max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8'>
+        <h1 className='text-3xl font-bold sm:text-4xl'>Portafolio</h1>
+        <p className='text-3xl font-bold sm:text-4xl'><strong>Jose Martin Reyes Franco</strong></p>
+        <p className='mt-4'>Aqui voy a poner mis proyecto que ire haciendo como desarrolador, para llevar un historial.</p>
+      </div>
       {/* //Proyectos */}
-      <div className=''>
+      <div className='mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3'>
         <ProyectoCard title='Rick and Morty Memorama'deploy='https://memorama-rick-and-morty.netlify.app/' github='https://github.com/MartiRF/React-RickAndMorty-Memorama' />
-        <ProyectoCard title='Pomodoro' deploy='https://martirf.github.io/pomodoro/' github='https://github.com/MartiRF/pomodoro'/>
+        <ProyectoCard title='Pomodoro' deploy='https://pomodoro-martidev.netlify.app' github='https://github.com/MartiRF/pomodoro'/>
+        <ProyectoCard title='Vacunacion CSS' deploy='https://martirf.github.io/vacunacionCSS_MisionFrontEnd/' github='https://github.com/MartiRF/vacunacionCSS_MisionFrontEnd' />
       </div>
     </div>
   )
